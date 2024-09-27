c892496852300d50e2d3a365ec29fd7309ba8a80
Sultepronit <sultepronit@gmail.com>
2024-09-27 13:51:47 +0300

    refactored

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['file'])) {
         $file = $_FILES['file'];
         $fileName = str_replace(' ', '_', $file['name']);
-        $destination = __DIR__ . "/audio-files/{$fileName}";
+        $destination = __DIR__ . "/audio/{$fileName}";
         print_r($file);
         echo PHP_EOL;
         print_r($destination);
@@ -22,17 +22,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $fileName = 'Elisa_-_Ebullient_Future.MP3';
+    // $fileName = 'Elisa_-_Ebullient_Future.MP3';
     // $fileName = '瀧川ありさ_『さよならのゆくえ』MUSIC_VIDEO(full_ver.).mkv';
-    $file = __DIR__ . "/audio-files/{$fileName}";
+    $fileName = '06_-_Fushigi_Tokyo_Cinderella.mp3';
+    $file = __DIR__ . "/audio/{$fileName}";
+    // echo $file;
     $contentType = mime_content_type($file);
     // echo $contentType;
-    header('Content-Description: File Transfer');
-    // header('Content-Type: application/octet-stream');
+    // header('Content-Description: File Transfer');
+    // header("Content-Transfer-Encoding: binary"); 
+    header('Accept-Ranges: bytes');
     header("Content-Type: {$contentType}");
-    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
+    // header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    // header('Expires: 0');
+    // header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
     readfile($file);
